67b58e998ea05a87f73ea95bf33aba693ecdb2b6
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-11-21 21:43:27 +0600

    second commit

--- C++ STL/main.cpp
+++ C++ STL/main.cpp
@@ -748,7 +748,7 @@
 // #include <bits/stdc++.h>
 // using namespace std;
 
-// bool comparator(pair<int, int> p1, pair<int, int> p2) {
+// bool comparator1(pair<int, int> p1, pair<int, int> p2) {
 // 	if (p1.second < p2.second) {
 // 		return true;
 // 	}
@@ -768,7 +768,7 @@
 
 // 	vector<pair<int, int>> vec = {{3, 1}, {2, 1}, {7, 1}, {5, 2}};
 
-// 	sort(vec.begin(), vec.end(), comparator);
+// 	sort(vec.begin(), vec.end(), comparator1);
 
 // 	for (auto p : vec) {
 // 		cout << p.first << " " << p.second << endl;
